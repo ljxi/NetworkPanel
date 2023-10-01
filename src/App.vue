dca1743d8b2f4af8358fabbf71862dafbb5a0400
Li-GanMa <admin@ljxnet.cn>
2023-10-01 13:03:00 +0800

    update
    update

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,20 @@
 <template>
   <el-container >
     <el-header style="height: fit-content;padding-bottom: 12px;background-color: #ffffff;">
-      <el-page-header style="margin-top: 10px;" title=" " :icon="null">
-        <template #content>
-          <div class="flex items-center">
-            <img :width="40" :height="40" class="mr-2" :src="header_icon" style="display:inline-block;vertical-align: -12px;"/>
-            <span style="color: #526484;font-size: 20px;font-weight: bolder;"> 网络面板 </span>
-          </div>
-        </template>
-        <template #extra>
-          <div class="flex items-center">
-            <el-button style="color: #526484;font-size: 12px;" @click="GoToHomePage" round>Q群</el-button>
-          </div>
-        </template>
-      </el-page-header>
+      <div style="margin-top: 15px;">
+        <div style="width: fit-content;float: left;">
+          <img :width="40" :height="40" class="mr-2" :src="header_icon" style="display:inline-block;vertical-align: -12px;"/>
+          <span style="color: #526484;font-size: 20px;font-weight: bolder;"> 网络面板 </span>
+        </div>
+        <div style="float: right;">
+          <el-button style="color: #526484;font-size: 12px;" @click="GoToHomePage" round>Q群</el-button>
+        </div>
+      </div>
     </el-header>
     <el-main>
-      <MainUI :isVisible="isVisible"/>
+      <MainUI :isVisible="isVisible" :IPinfo="IPinfo"/>
       <br>
-      <IPinfo :isVisible="isVisible"/>
+      <IPinfoUI :isVisible="isVisible" :IPinfo="IPinfo"/>
     </el-main>
     <div style="height: fit-content;padding-bottom: 10px;">
       <div style="width: fit-content;margin-left: auto;margin-right: auto;">
@@ -30,27 +26,27 @@
 </template>
 
 <script lang="ts" setup>
+// import { useDark, useToggle } from '@vueuse/core'
+
+// const isDark = useDark()
+// const toggleDark = useToggle(isDark)
+// toggleDark()
 import MainUI from "./components/Main.vue"
-import IPinfo from "./components/IPinfo.vue"
+import IPinfoUI from "./components/IPinfo.vue"
 import header_icon from "./assets/logo.svg"
-import { ref } from 'vue'
+import { ref , reactive} from 'vue'
 import { ElMessage } from 'element-plus'
-import { el } from "element-plus/es/locale/index.mjs"
+import { toClipboard } from '@soerenmartius/vue3-clipboard'
+
 const isVisible=ref(true)
+const IPinfo=reactive({globalInfo:null,localInfo:null})
 let GoToHomePage =  ()=> { 
   //window.open("https://netart.cn/")
-  if (navigator.clipboard) {
-      navigator.clipboard.writeText('463481772');
-      ElMessage.info({
-        dangerouslyUseHTMLString: true,
-        message: '<center>已经复制QQ群号：<br><strong>463481772</strong></center>',
-      })
-  }else{
-    ElMessage.info({
-      dangerouslyUseHTMLString: true,
-      message: '<center>QQ群号：<br><strong>463481772</strong></center>',
-    })
-  }
+  toClipboard('463481772')
+  ElMessage.info({
+    dangerouslyUseHTMLString: true,
+    message: '<center>已经复制QQ群号：<br><strong>463481772</strong></center>',
+  })
 }
 document.addEventListener("visibilitychange", function() {
   var string = document.visibilityState
---
+++ src/components/My.vue
@@ -0,0 +1,160 @@
+<script setup lang="ts">
+import {ref,watch,onMounted, onUnmounted} from 'vue'
+import { ElMessage, ElMessageBox } from 'element-plus'
+const props = defineProps({
+  loginInfo: { type: Object, required: true },
+})
+const loginStatus=ref(0)
+const imgBase64=ref("")
+
+
+watch(loginStatus,(ns,os)=>{
+  if(ns<=0)props.loginInfo.AccessToken=''
+})
+const api =async(args:object)=>{
+  const response = await fetch('https://api.netart.cn/user/', {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    redirect: "follow",
+    referrerPolicy: "no-referrer",
+    body: JSON.stringify(args)
+})
+const resp=await response.json()
+return resp
+}
+let checkTsk:number=0;
+const login=async()=>{
+  imgBase64.value=''
+  let resp=await api({action:'login',AccessToken:props.loginInfo.AccessToken})
+  loginStatus.value=-1
+  if(resp.status==0){
+    imgBase64.value=resp.img
+    sessionStorage.setItem("pt_login_sig", resp["pt_login_sig"]);
+    sessionStorage.setItem("qrsig", resp["qrsig"]);
+    checkTsk=setInterval(qr_check,1000)
+  }
+}
+const kick_old=async()=>{
+  ElMessageBox.confirm(
+    '确定要强制下线登录该账号的其他设备吗？',
+    '提示',
+    {
+      confirmButtonText: '确定',
+      cancelButtonText: '取消',
+      type: 'warning',
+    }
+  )
+    .then(async() => {
+      let resp=await api({action:'kick_old',AccessToken:props.loginInfo.AccessToken})
+      if(resp.status==0){
+        ElMessage({
+            type: 'info',
+            message: `已经下线其他设备`,
+        })
+      }
+    })
+    .catch(() => {})
+}
+const logOut=async()=>{
+  ElMessageBox.confirm(
+    '确定从此设备退出登录吗？',
+    '提示',
+    {
+      confirmButtonText: '确定',
+      cancelButtonText: '取消',
+      type: 'warning',
+    }
+  )
+    .then(() => {
+      login()
+    })
+    .catch(() => {})
+}
+const qr_check=async()=>{
+  let resp=await api({action:'qr_check',
+                      pt_login_sig:sessionStorage.getItem("pt_login_sig"),
+                      qrsig:sessionStorage.getItem("qrsig"),
+                    })
+  if(resp.status==0){
+    loginStatus.value=resp.uin
+    props.loginInfo.AccessToken=resp.AccessToken
+    clearInterval(checkTsk)
+  }else if(resp.status==-2){
+    loginStatus.value=-2
+  }else if(resp.status==-1){
+    loginStatus.value=-1
+  }else if(resp.status==-10){
+    clearInterval(checkTsk)
+    login()
+  }
+}
+const getStatus=async()=>{
+  let resp=await api({action:'getStatus',AccessToken:props.loginInfo.AccessToken})
+  if(resp.status==0){
+    loginStatus.value=resp.uin
+  }else if(resp.status == -1){
+    loginStatus.value=-1
+    login()
+  }
+}
+onMounted(() => {
+  console.log("M")
+  getStatus()
+})
+
+onUnmounted(() =>{
+  console.log("UM")
+  checkTsk?clearInterval(checkTsk):""
+  })
+</script>
+
+<template>
+  <div class="block">
+    <div v-loading="!loginStatus" class="card">
+      <div v-if="loginStatus==-1 || loginStatus==-2">
+        <div v-loading="!imgBase64" style="width: 150px;height: 150px;margin-left: auto;margin-right: auto;">
+          <img style="width: 100%;" v-if="imgBase64" :src="imgBase64"/>
+        </div>
+        <br>
+        <span v-if="loginStatus!=-2" style="font-size: 15px;">请使用QQ扫码登录</span>
+        <br>
+        <span v-if="loginStatus!=-2" style="font-size: 15px;">TIM可以从相册扫码</span>
+        <span v-if="loginStatus==-2" style="font-size: 15px;">请确认登录</span>
+      </div>
+      <div v-if="loginStatus>0">
+        <el-avatar :size="100" :src="'https://q.qlogo.cn/headimg_dl?dst_uin='+loginStatus+'&spec=640'" />
+        <br>
+        <span style="font-size: 20px;">{{loginStatus}}</span>
+        <el-divider />
+        <el-button class='action' type="warning" @click="kick_old">下线其他设备</el-button>
+        <el-button class='action' type="danger" @click="logOut">退出登录</el-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.block{
+  height: fit-content;
+}
+.card{
+  width: fit-content;
+  height: fit-content;
+  padding: 2%;
+  background-color:azure;
+  margin-left: auto;
+  margin-right: auto;
+  border-radius: 50px;
+  text-align: center; 
+  padding: 5%;
+}
+.action{
+  margin-bottom: 20px;
+  width: 100px;
+  margin-left: 12px;
+  margin-right: 12px;
+}
+</style>

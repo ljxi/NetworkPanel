8d7c38d9f8acf60da9d9d25be788720d2e3ac3d0
Li-GanMa <admin@ljxnet.cn>
2023-12-22 17:16:53 +0800

    图标优化

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,17 @@
     <el-header class="header">
       <div style="margin-top: 15px;">
         <div style="width: fit-content;float: left;">
-          <svg
-            style="display:inline-block;vertical-align: -12px;width: 40px;height: 40px;"
-            t="1702892592527" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
-            p-id="4460" width="200" height="200" data-spm-anchor-id="a313x.search_index.0.i9.1e343a81LSbmza">
-            <path
-              d="M568.96 459.242667l144-189.205334A382.208 382.208 0 0 0 512 213.333333c-212.074667 0-384 171.925333-384 384a382.293333 382.293333 0 0 0 93.397333 251.008l-64.554666 55.808A467.584 467.584 0 0 1 42.666667 597.333333C42.666667 338.133333 252.8 128 512 128c93.098667 0 179.861333 27.093333 252.842667 73.856l75.882666-99.690667 67.541334 51.413334-273.28 359.04a149.333333 149.333333 0 1 1-66.048-53.376z m266.453333-69.056l54.357334-71.424A467.242667 467.242667 0 0 1 981.333333 597.333333c0 113.706667-40.64 221.226667-113.237333 305.728l-64.725333-55.616A382.272 382.272 0 0 0 896 597.333333c0-76.288-22.250667-147.370667-60.586667-207.146666zM512 661.333333a64 64 0 1 0 0-128 64 64 0 0 0 0 128z"
-              fill="rgb(80,170,255)" p-id="4461"></path>
-          </svg>
+          <div class="icon">
+            <svg t="1702892592527" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
+              p-id="4460" width="200" height="200" data-spm-anchor-id="a313x.search_index.0.i9.1e343a81LSbmza">
+              <path
+                d="M568.96 459.242667l144-189.205334A382.208 382.208 0 0 0 512 213.333333c-212.074667 0-384 171.925333-384 384a382.293333 382.293333 0 0 0 93.397333 251.008l-64.554666 55.808A467.584 467.584 0 0 1 42.666667 597.333333C42.666667 338.133333 252.8 128 512 128c93.098667 0 179.861333 27.093333 252.842667 73.856l75.882666-99.690667 67.541334 51.413334-273.28 359.04a149.333333 149.333333 0 1 1-66.048-53.376z m266.453333-69.056l54.357334-71.424A467.242667 467.242667 0 0 1 981.333333 597.333333c0 113.706667-40.64 221.226667-113.237333 305.728l-64.725333-55.616A382.272 382.272 0 0 0 896 597.333333c0-76.288-22.250667-147.370667-60.586667-207.146666zM512 661.333333a64 64 0 1 0 0-128 64 64 0 0 0 0 128z"
+                fill="#ffffff" p-id="4461"></path>
+            </svg>
+          </div>
           <span class="title"> 网络面板 </span>
         </div>
-        <div style="float: right;">
+        <div style="float: right;margin-top: 5px;">
           <el-button class="home" @click="GoToHomePage" round>反馈</el-button>
         </div>
       </div>
@@ -74,7 +74,18 @@ document.addEventListener("visibilitychange", function () {
   color: #526484;
   font-size: 12px;
 }
-
+.icon {
+  display:inline-block;
+  vertical-align: -6px;
+  width: 40px;
+  height: 40px;
+  background-color: #5668EE;
+  border-radius: 20%;
+}
+.icon svg {
+  width: 80%;
+  margin-left: 10%;
+}
 @media (prefers-color-scheme: dark) {
   .header {
     background-color: rgb(18, 18, 18);

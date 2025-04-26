4e0f7af8670d04aad3349bb3b17100fc4328e119
ljxi <582424565@qq.com>
2025-04-26 18:51:28 +0800

    app3.2.2

--- src/App.vue
+++ src/App.vue
@@ -55,8 +55,8 @@ import { ElMessage } from 'element-plus'
 import { toClipboard } from '@soerenmartius/vue3-clipboard'
 const native=window.mjs
 const isVisible = ref(true)
-const newVersionCode = 321
-const newVersionName = "3.2.1"
+const newVersionCode = 322
+const newVersionName = "3.2.2"
 const versionCode = ref(0)
 const versionName = ref("")
 const updateDialog = ref(false)
